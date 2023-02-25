d5af759a96c4258cf65a8fb2ab069577b1ae5a32
Gerald Omondi <geraldomondi30@gmail.com>
2023-02-25 11:25:28 +0300

    first commit

---
+++ routes/authRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+import authenticateUser from "../middleware/auth.js";
+const authRouter= express.Router();
+
+import { login,updateUser,register } from "../controllers/authController.js";
+
+
+authRouter.route('/register').post(register);
+authRouter.route('/login').post(login)
+authRouter.route('/updateUser').patch(authenticateUser,updateUser);
+
+
+export default authRouter;
